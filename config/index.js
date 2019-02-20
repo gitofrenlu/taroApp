41268169dd82a1d2c319adb5f74f6b9ea4250a8c
ying.lin <ying.lin@mljr.com>
2019-02-20 10:48:02 +0800

    改成redux

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const config = {
-  projectName: 'myTarAp',
-  date: '2019-2-18',
+  projectName: 'taroApp',
+  date: '2019-2-20',
   designWidth: 750,
   deviceRatio: {
     '640': 2.34 / 2,
--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,10 @@
+import '@tarojs/async-await'
 import Taro, { Component, Config } from '@tarojs/taro'
+import { Provider } from '@tarojs/redux'
+
 import Index from './pages/index'
+
+import configStore from './store'
 
 import './app.scss'
 
@@ -8,6 +13,8 @@ import './app.scss'
 // if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
 //   require('nerv-devtools')
 // }
+
+const store = configStore()
 
 class App extends Component {
 
@@ -36,13 +43,17 @@ class App extends Component {
 
   componentDidHide () {}
 
+  componentCatchError () {}
+
   componentDidCatchError () {}
 
   // 在 App 类中的 render() 函数没有实际作用
   // 请勿修改此函数
   render () {
     return (
-      <Index />
+      <Provider store={store}>
+        <Index />
+      </Provider>
     )
   }
 }
